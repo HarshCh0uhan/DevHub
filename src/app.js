b835d843b57015829548a4b9b66621f6270b6af8
HarshCh0uhan <harshcoding2004@gmail.com>
2025-01-27 11:16:25 +0530

    dynamic data in sighnup api

--- src/app.js
+++ src/app.js
@@ -6,15 +6,10 @@ const {connectDB} = require("./config/db")
 
 const {User} = require("./models/user")
 
+app.use(express.json())
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Harsh",
-        lastName: "Chouhan",
-        emsilId: "harsh@chouhan.com",
-        password: "harsh@123",
-        age: 20,
-        gender: "Male",
-    });
+    const user = new User(req.body);
 
     try{
         await user.save()
